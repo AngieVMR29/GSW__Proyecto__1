62995d7c991044bae3f630e5743db4a2878f46d5
Angie Mendieta <angie.mendieta9@misena.edu.co>
2024-03-03 00:09:58 -0500

    Se genera modulo crear y eliminar usuarios en la base administrador

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,  render_template, request, redirect, url_for, session, flash # pip install Flask
 from flask_mysqldb import MySQL,MySQLdb # pip install Flask-MySQLdb
+from werkzeug.utils import secure_filename
 from os import path #pip install notify-py
 from notifypy import Notify
 
@@ -10,17 +11,24 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'greensoftworld'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['UPLOAD_FOLDER'] = './static/uploads'
+ALLOWED_EXTENSIONS = set(['png','jpg','jpng','gif'])
 mysql = MySQL(app)
 
 @app.route('/')
 def home():
     return render_template('/inicio.html')
 
+@app.route('/404')
+def not_found():
+    return 'url no encontrada'
+
 @app.route('/registrogsw', methods = ["GET", "POST"])
 def registrogsw():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM tipo_documento")
     tipo = cur.fetchall()
+    print(tipo)
 
     return render_template('/registrate.html', tipos = tipo)
 
@@ -31,6 +39,7 @@ def iniciogsw():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+
         cur = mysql.connection.cursor()
         cur.execute("SELECT * FROM persona WHERE Email = %s AND Password = %s",(email, password,))
         persona = cur.fetchone()
@@ -64,30 +73,71 @@ def iniciogsw():
 def administrador():
     return render_template('administrador/administrador.html')
 
-@app.route('/basedeusuarios', methods=['GET','POST'])
+@app.route('/basedeusuarios')
 def basedeusuarios():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM persona')
+    persona = cur.fetchall()
+    return render_template('administrador/usuarios/basedeusuarios.html', personas = persona)
+
+
+@app.route('/crearusuario', methods=['GET','POST'])
+def crear_persona():
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM tipo_documento")
+    tipo = cur.fetchall()
+    
+    if request.method == 'POST':
+        Nombres = request.form['Nombres']
+        Apellidos = request.form['Apellidos']
+        Tipo_Doc = request.form['tipo']
+        Documento = request.form['Documento']
+        Email = request.form['Email']
+        foto = request.files['foto']
+        Password = request.form['Password']
+        direccion = request.form['direccion']
+
         cur = mysql.connection.cursor()
-        cur.execute('SELECT * FROM persona')
-        persona = cur.fetchall()
-        return render_template('administrador/basedeusuarios.html', personas = persona)
+        cur.execute(" INSERT INTO persona (Nombres, Apellidos,Tipo_Doc, Documento, Email, Password, foto, direccion, estado) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",(Nombres, Apellidos,Tipo_Doc, Documento, Email, Password,foto,direccion,True))
+        cur.connection.commit()
+
+        flash('Persona creado exitosamente')
+        return render_template('administrador/usuarios/basedeusuarios.html')
+    return render_template('administrador/usuarios/crearusuario.html',  tipos = tipo)
+    
+    
 
 @app.route('/eliminar/<string:id>')
 def eliminar_persona(id):
         cur = mysql.connection.cursor()
-        cur.execute('DELETE * FROM persona WHERE Id_Persona = %s',(id,))
+        cur.execute('DELETE FROM persona WHERE Id_Persona = %s',(id,))
         cur.connection.commit()
-        flash('contacto eliminado exitosamente')
+        flash('Persona eliminada exitosamente')
         return redirect(url_for('basedeusuarios'))
 
 
-@app.route('/editar/<string:id>')
+@app.route('/editar/<string:id>', methods=['GET','POST'])
 def editar_persona(id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM persona WHERE Id_Persona = %s',(id,))
-    persona = cur.fetchall()
-    print(persona[0])
-    return 'recibido'
+    if request.method == 'POST':
+        Nombres = request.form['Nombres']
+        Apellidos = request.form['Apellidos']
+        Documento = request.form['Documento']
+        Email = request.form['Email']
+        Password = request.form['Password']
 
+        cur = mysql.connection.cursor()
+        cur.execute("""UPDATE persona SET 
+                Nomres = %s, 
+                Apellidos = %s, 
+                Documento = %s, 
+                Email= %s, 
+                Password = %s
+                WHERE Id_Persona = %s
+                """,(Nombres, Apellidos, Documento, Email, Password,))
+        flash('Actualización de datos exitosa')
+        return redirect(url_for('basedeusuarios'))
+    
 @app.route('/basedepublicaciones', methods=['GET','POST'])
 def basedepublicaciones():
     return render_template('administrador/administrador.html')
@@ -104,4 +154,4 @@ def buscar():
 
 if __name__ == '__main__':
     app.secret_key = "Hola1234."
-    app.run(port=3000 ,debug=True)
+    app.run(debug=True)
